e2bc498560fe187748abb276582217856b777d84
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-16 16:50:04 +0100

    update

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
 
 <div class="container">
   <div class="row">
-    <div class="col-md-6 one">
+    <div class="span6 one">
       <div class="cont">
         1
       </div>
     </div>
-    <div class="col-md-6 two">
+    <div class="span6 two">
       <div class="cont">
         2
       </div>
