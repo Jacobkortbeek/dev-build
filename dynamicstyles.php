326e183a477b05675d4bdb101a962c056c7cff2a
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-16 21:58:21 +0100

    body colour

--- dynamicstyles.php
+++ dynamicstyles.php
@@ -1,5 +1,5 @@
 <style type="text/css">
-<?php if ( get_field( 'background_choice', 'options' ) == 'single' ): ?>
+<?php if ( get_field( 'background_choice', 'options' ) == 'single_color' ): ?>
   .body{
     background-color: <?php the_field( 'background_colour', 'option' ); ?>;
   }
