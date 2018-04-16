d8ceb11a87e1df2b50d08f882ad414b096f2bcaa
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-16 22:54:17 +0100

    body colour

--- dynamicstyles.php
+++ dynamicstyles.php
@@ -5,19 +5,7 @@
      <?php the_field( 'background_colour', 'option' ); ?>;
      <?php elseif ( get_field( 'background_choice', 'options' ) == 'gradient' ): ?>
       linear-gradient(
-        <?php if ( get_field( 'gradient_colour_one', 'option' ) ): ?>
-          <?php the_field( 'gradient_colour_one', 'option' ); ?>,
-        <?php endif; ?>
-        <?php if ( get_field( 'gradient_colour_two', 'option' ) ): ?>
-          <?php the_field( 'gradient_colour_two', 'option' ); ?>,
-        <?php endif; ?>
-        <?php if ( get_field( 'gradient_colour_three', 'option' ) ): ?>
-          <?php the_field( 'gradient_colour_three', 'option' ); ?>,
-        <?php endif; ?>
-        <?php if ( get_field( 'gradient_colour_four', 'option' ) ): ?>
-          <?php the_field( 'gradient_colour_four', 'option' ); ?>,
-        <?php endif; ?>
-         );
+        <?php the_field( 'gradient_colour_one', 'option' ); ?>, <?php the_field( 'gradient_colour_two', 'option' ); ?>, <?php the_field( 'gradient_colour_three', 'option' ); ?>, <?php the_field( 'gradient_colour_four', 'option' ); ?>);
      <?php endif ;?>
      background-repeat: no-repeat;
   }
