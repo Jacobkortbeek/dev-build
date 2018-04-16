1ea3cafa93a7537f5c6ba1074af089230ced42e4
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-16 21:48:04 +0100

    body colour

--- dynamicstyles.php
+++ dynamicstyles.php
@@ -1,5 +1,5 @@
 <style type="text/css">
-<?php if( get_field( 'background_choice' ) == 'single' ): ?>
+<?php if( get_field( 'background_choice' ) == 'gradient' ): ?>
   .body{
     background-color: <?php the_field( 'background_colour' ); ?>;
   }
