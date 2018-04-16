e5931d062882b1eadf5c79e464743406f8df3476
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-16 21:27:38 +0100

    body colour

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
     <div class="container">
       <div class="row">
         <div class="col-12 header">
-          <h2><?php the_field( 'head_title', 'option' ); ?></h2>
+          <h2 class="heading"><?php the_field( 'head_title', 'option' ); ?></h2>
         </div>
       </div>
     </div>
