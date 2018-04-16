2b2f98ec2560c631d1e845fdfa911420821bd406
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-16 16:12:36 +0100

    style

--- index.php
+++ index.php
@@ -3,10 +3,14 @@
 <div class="container">
   <div class="row">
     <div class="col-md-6 one">
+      <div class="cont">
 
+      </div>
     </div>
     <div class="col-md-6 two">
+      <div class="cont">
 
+      </div>
     </div>
   </div>
 </div>
