da52d767ac1e121899def22b55860e47bfbcea10
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-16 22:52:27 +0100

    body colour

--- dynamicstyles.php
+++ dynamicstyles.php
@@ -4,7 +4,20 @@
     background:<?php if ( get_field( 'background_choice', 'options' ) == 'single_color' ): ?>
      <?php the_field( 'background_colour', 'option' ); ?>;
      <?php elseif ( get_field( 'background_choice', 'options' ) == 'gradient' ): ?>
-      linear-gradient(<?php the_field( 'gradient_colour_one', 'option' ); ?>, <?php the_field( 'gradient_colour_two', 'option' ); ?>, <?php the_field( 'gradient_colour_three', 'option' ); ?>, <?php the_field( 'gradient_colour_four', 'option' ); ?>);
+      linear-gradient(
+        <?php if ( get_field( 'gradient_colour_one', 'option' ) ): ?>
+        <?php the_field( 'gradient_colour_one', 'option' ); ?>,
+        <?php endif; ?>
+        <?php if ( get_field( 'gradient_colour_two', 'option' ) ): ?>
+        <?php the_field( 'gradient_colour_two', 'option' ); ?>,
+        <?php endif; ?>
+        <?php if ( get_field( 'gradient_colour_three', 'option' ) ): ?>
+        <?php the_field( 'gradient_colour_three', 'option' ); ?>,
+        <?php endif; ?>
+        <?php if ( get_field( 'gradient_colour_four', 'option' ) ): ?>
+        <?php the_field( 'gradient_colour_four', 'option' ); ?>,
+        <?php endif; ?>
+         );
      <?php endif ;?>
      background-repeat: no-repeat;
   }
