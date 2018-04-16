906ad7753f98c35879503a3f2f022f2acf6925e4
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-16 22:08:00 +0100

    body colour

--- dynamicstyles.php
+++ dynamicstyles.php
@@ -4,7 +4,7 @@
     background-color:<?php if ( get_field( 'background_choice', 'options' ) == 'single_color' ): ?>
      <?php the_field( 'background_colour', 'option' ); ?>;
      <?php elseif ( get_field( 'background_choice', 'options' ) == 'gradient' ): ?>
-      linear-gradient(<?php the_field( 'gradient_colour_one' ); ?>, <?php the_field( 'gradient_colour_two' ); ?>, <?php the_field( 'gradient_colour_three' ); ?>, <?php the_field( 'gradient_colour_four' ); ?>);
+      linear-gradient(<?php the_field( 'gradient_colour_one', 'option' ); ?>, <?php the_field( 'gradient_colour_two', 'option' ); ?>, <?php the_field( 'gradient_colour_three', 'option' ); ?>, <?php the_field( 'gradient_colour_four', 'option' ); ?>);
      <?php endif ;?>
   }
 
