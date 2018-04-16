86c88df2da5b7ace7c9c6a890051dee47d83121a
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-16 16:29:51 +0100

    style

---
+++ header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title><?php wp_title( '|', true, 'right' ); bloginfo( 'name' ); ?></title>
+    <?php wp_head(); ?>
+  </head>
+  <header>
+  </header>
+
+  <body>
