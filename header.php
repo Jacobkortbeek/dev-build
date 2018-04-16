3a6b506dc33c62d8fc0c18ec72393beb553d433d
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-16 19:50:38 +0100

    update

--- header.php
+++ header.php
@@ -9,9 +9,7 @@
     <div class="container">
       <div class="row">
         <div class="col-12 header">
-          <?php get_field( 'head_title' ); ?>
-          <h2><?php the_field( 'head_title' ); ?></h2>
-          
+          <h2><?php get_field( 'head_title', 'option' ); ?></h2>
         </div>
       </div>
     </div>
