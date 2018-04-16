63c05c93a0ba0f1dcbc9cd8681b3d5ca32c23334
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-16 20:29:48 +0100

    body colour

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <?php wp_head(); ?>
   </head>
   <header>
-    <?php include( 'global_styles' ); ?>
+    <?php include( './global_styles' ); ?>
     <div class="container">
       <div class="row">
         <div class="col-12 header">
