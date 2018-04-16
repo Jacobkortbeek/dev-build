766669289a282bafa69c75336cac0969291f69fe
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-16 23:40:25 +0100

    heading options

--- dynamicstyles.php
+++ dynamicstyles.php
@@ -27,7 +27,7 @@
 .heading {
   padding: 20px;
   <?php if ( get_field( 'heading_colour', 'option' ) ) : ?>
-    color: <?php the_field( 'heading_colour', 'option' ); ?>,
+    color: <?php the_field( 'heading_colour', 'option' ); ?>;
   <?php endif; ?>
 }
 </style>
