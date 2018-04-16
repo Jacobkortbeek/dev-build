afd997304b2f1170af2fbc694421ae3eb7284eb5
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-16 18:02:43 +0100

    update

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
 
   function wpt_theme_styles() {
     wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'bootstrap_responsive', get_template_directory_uri() . '/css/bootstrap-responsive.min.css' );
+    wp_enqueue_style( 'bootstrap_responsive', get_template_directory_uri() . '/css/bootstrap-theme.min.css' );
     wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
   }
   add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
