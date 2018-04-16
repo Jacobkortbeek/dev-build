99d949903772708d2fc1fa30395deab2f1cba1a2
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-16 23:04:38 +0100

    body colour

--- dynamicstyles.php
+++ dynamicstyles.php
@@ -1,11 +1,23 @@
 <style type="text/css">
 
   body{
-    background:<?php if ( get_field( 'background_choice', 'options' ) == 'single_color' ): ?>
+    background:
+    <?php if ( get_field( 'background_choice', 'options' ) == 'single_color' ): ?>
      <?php the_field( 'background_colour', 'option' ); ?>;
-     <?php elseif ( get_field( 'background_choice', 'options' ) == 'gradient' ): ?>
+    <?php elseif ( get_field( 'background_choice', 'options' ) == 'gradient' ): ?>
       linear-gradient(
+      <?php if ( get_field( 'number_of_colours_in_gradient', 'option' ) == 'four' ) : ?>
         <?php the_field( 'gradient_colour_one', 'option' ); ?>, <?php the_field( 'gradient_colour_two', 'option' ); ?>, <?php the_field( 'gradient_colour_three', 'option' ); ?>, <?php the_field( 'gradient_colour_four', 'option' ); ?>);
+      <?php endif; ?>
+
+      <?php if ( get_field( 'number_of_colours_in_gradient', 'option' ) == 'three' ) : ?>
+        <?php the_field( 'gradient_colour_one', 'option' ); ?>, <?php the_field( 'gradient_colour_two', 'option' ); ?>, <?php the_field( 'gradient_colour_three', 'option' ); ?>);
+      <?php endif; ?>
+
+      <?php if ( get_field( 'number_of_colours_in_gradient', 'option' ) == 'two' ) : ?>
+        <?php the_field( 'gradient_colour_one', 'option' ); ?>, <?php the_field( 'gradient_colour_two', 'option' );?>);
+      <?php endif; ?>
+
      <?php endif ;?>
      background-repeat: no-repeat;
   }
