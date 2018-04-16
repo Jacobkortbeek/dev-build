c8844d2018f17ec01ab79549d374457116289c3b
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-16 21:40:14 +0100

    body colour

--- dynamicstyles.php
+++ dynamicstyles.php
@@ -1,9 +1,9 @@
 <style type="text/css">
-<?php if ( get_field( 'background_choice' ) == 'single_color' ); ?>
+<?php if( get_field( 'background_choice' ) == 'single_color' ): ?>
   .body{
     background-color: <?php the_field( 'background_colour' ); ?>;
   }
-<?php endif ?>
+<?php endif ;?>
 
 .heading {
   color: <?php echo get_field( 'heading_colour', 'option' ); ?>;
