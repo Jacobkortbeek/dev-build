c5094e5f16f0cf2b36e38a08ed505eb00cdf2e2e
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-16 18:12:01 +0100

    update

--- header.php
+++ header.php
@@ -6,6 +6,13 @@
     <?php wp_head(); ?>
   </head>
   <header>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 header">
+          <h2><?php the_field( 'head_title' ); ?></h2>
+        </div>
+      </div>
+    </div>
   </header>
 
   <body>
