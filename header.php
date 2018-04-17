7ae37e3066c03fc5285f85705eb9132a17c27699
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-17 01:39:03 +0100

    heading options

--- header.php
+++ header.php
@@ -10,10 +10,8 @@
     <div class="container">
       <div class="row">
         <div class="col-12 header">
-          <<?php
-              $text = get_field( 'text_options', 'options' );
-              if( $text ): ?>
-              <?php echo $text['heading_h_tag']; ?>
+          <<?php $text = get_field( 'text_options', 'options' );
+              if( $text ): ?><?php echo $text['heading_h_tag']; ?>
            class="heading"><?php the_field( 'head_title', 'option' ); ?></<?php echo $text['heading_h_tag']; ?>  <?php endif; ?> >
         </div>
       </div>
