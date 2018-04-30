dbb394da76535426ecdc6472cf2a1b65cd141322
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-04-30 17:13:53 +0100

    test

--- index.php
+++ index.php
@@ -12,16 +12,9 @@
     </div>
     <div class="col-md-6 two">
       <div class="cont">
-        <?php
-          $text = get_field( 'text_options', 'options' );
-
-          if( $text ): ?>
-          <?php echo $text['main_heading_colour']; ?>
-          <?php echo $text['heading_font_size']; ?>em
-          <?php echo $text['header_line_hieght']; ?>px
-          <?php echo $text['header_font_weight']; ?>
-          <?php echo $text['heading_h_tag']; ?>
-        <?php endif; ?>
+        <div class="sub-heading">
+          <h3>sub heading</h3>
+        </div>
       </div>
     </div>
   </div>
