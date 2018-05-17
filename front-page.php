07afbba9b14da5255b011473eef8b812577d04a3
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-05-17 11:48:40 +0100

    front page

--- front-page.php
+++ front-page.php
@@ -1,5 +1,14 @@
 <?php get_header(); ?>
 
-
+<div class="container">
+  <div class="row">
+    <div class="col-md-12 logo-hero">
+      <?php $band_logo = get_field( 'band_logo', 'options' ); ?>
+      <?php if ( $band_logo ) { ?>
+         <img src="<?php echo $band_logo['url']; ?>" alt="<?php echo $band_logo['alt']; ?>" />
+      <?php } ?>
+    </div>
+  </div>
+</div>
 
 <?php get_footer(); ?>
