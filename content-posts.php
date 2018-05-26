23a149f549cddf617a9960c553de2011c9442f7d
jacobkortbeek <kortbeekdesigns@gmail.com>
2018-05-26 23:01:33 +0100

    home blog posts

--- content-posts.php
+++ content-posts.php
@@ -18,11 +18,14 @@
 
   <?php if( $query->have_posts() ) : while( $query->have_posts() ) : $query->the_post(); ?>
     <div class="col-md-4 blogHomePost">
-      <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'large' ); ?></a>
-      <br>
-      <h1><?php the_title(); ?></h1>
-      <br>
-      <p><?php echo strip_tags( get_the_excerpt() ); ?></p>
+      <div class="padding">
+        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'large' ); ?></a>
+        <br>
+        <h1><?php the_title(); ?></h1>
+        <br>
+        <p><?php echo strip_tags( get_the_excerpt() ); ?></p>
+      </div>
+
     </div>
   <?php endwhile; endif; wp_reset_postdata(); ?>
   </div>
